36c1c7435b5f140fa705da1e6301c7ceffb10eaa
hubert <gsj285@umsystem.edu>
2021-01-19 00:34:21 -0600

    Add file and rank arrays and type. And use them to get square array and type.

--- src/types.ts
+++ src/types.ts
@@ -18,18 +18,14 @@ export const pieceCodes = [ 'K', 'R', 'B', 'G', 'S', 'N', 'L', 'P',
                             '+R', '+B', '+S', '+N', '+L', '+P',
                             '+r', '+b', '+s', '+n', '+l', '+p', ] as const;
 
-export const squares = [ '9a' , '8a' , '7a' , '6a' , '5a' , '4a' , '3a' , '2a' , '1a' ,
-                         '9b' , '8b' , '7b' , '6b' , '5b' , '4b' , '3b' , '2b' , '1b' ,
-                         '9c' , '8c' , '7c' , '6c' , '5c' , '4c' , '3c' , '2c' , '1c' ,
-                         '9d' , '8d' , '7d' , '6d' , '5d' , '4d' , '3d' , '2d' , '1d' ,
-                         '9e' , '8e' , '7e' , '6e' , '5e' , '4e' , '3e' , '2e' , '1e' ,
-                         '9f' , '8f' , '7f' , '6f' , '5f' , '4f' , '3f' , '2f' , '1f' ,
-                         '9g' , '8g' , '7g' , '6g' , '5g' , '4g' , '3g' , '2g' , '1g' ,
-                         '9h' , '8h' , '7h' , '6h' , '5h' , '4h' , '3h' , '2h' , '1h' ,
-                         '9i' , '8i' , '7i' , '6i' , '5i' , '4i' , '3i' , '2i' , '1i' ] as const;
+export const files = ['9', '8', '7', '6', '5', '4', '3', '2', '1'] as const;
+export const ranks = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'] as const;
+export const squares = Array.prototype.concat(...ranks.map(r => files.map(f => f+r)));
 
 export type Piececode = typeof pieceCodes[number];
-export type Square = typeof squares[number];
+export type File = typeof files[number];
+export type Rank = typeof ranks[number];
+export type Square = `${File}${Rank}`;
 
 export interface Piece {
     type: Piecetype,
